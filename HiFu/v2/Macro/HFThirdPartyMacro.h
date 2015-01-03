2658c0180e44339ffc1458635109bc5e64ebf4f0
Peng Wan <mrpengwan@gmail.com>
2015-01-02 22:52:05 -1000

    qq login

--- HiFu/v2/Macro/HFThirdPartyMacro.h
+++ HiFu/v2/Macro/HFThirdPartyMacro.h
@@ -1,7 +1,7 @@
 
 #define WECHAT_APP_KEY @"wxd930ea5d5a258f4f"
 
-#define QQ_APP_KEY @"222222"
+#define QQ_APP_KEY @"ogjJlgbQ9p5N6DRq"
 #define QQ_APP_RedirectURI @"www.qq.com"
 
 //weibo login and share
